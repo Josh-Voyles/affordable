ffe9b66c5160c26c629019905bf46bc96f4d4f28
Josh Voyles <soundtech821@gmail.com>
2024-06-06 15:12:05 -0400

    See: #22 #31, add initial gui from auto-gen, add resources auto-gen

--- home-choice-pro/main.py
+++ home-choice-pro/main.py
@@ -1,3 +1,20 @@
 """
 This file will serve as the main entry point to the program.
 """
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from views.main_window_ui import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
