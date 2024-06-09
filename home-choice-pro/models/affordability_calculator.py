7826d296f31513bc0c5bc1d8453a13240e2922b1
Randy Shreeves <shreevesrandy@gmail.com>
2024-06-08 23:05:02 -0600

    Update affordability_calculator.py for issue #20
    Removed parse functions, aimed towards pure functions, broke large
    functions into smaller functions, and ensured all calculator functions
    return Strings. Also updated test file to test all updated and new
    functions.

--- home-choice-pro/models/affordability_calculator.py
+++ home-choice-pro/models/affordability_calculator.py
@@ -1,69 +1,203 @@
 import math
 
+
 class AffordabilityCalculator:
 
-    def parse_interest_rate(self, interest_rate_string):
-        try:
-            return float(interest_rate_string) / 100 / 12
-        except ValueError:
-            raise ValueError("Error Interest Rate")
+    # Class Variables
+    monthly_payment: float = -1.0
+    down_payment: float = -1.0
+    interest_rate: float = -1.0
+    loan_term: float = -1.0
+    home_affordability_price: int = -1
 
-    def parse_term(self, term_index):
-        if term_index == 0:
-            return 180  # term in months
-        return 360  # term in months
+    # Getters
+    def get_monthly_payment(self) -> float:
+        return self.monthly_payment
 
-    def parse_monthly_payment(self, monthly_payment_string):
-        try:
-            return float(monthly_payment_string)
-        except ValueError:
-            raise ValueError("Error Monthly Payment")
+    def get_down_payment(self) -> float:
+        return self.down_payment
 
-    def parse_down_payment(self, down_payment_string):
-        try:
-            return float(down_payment_string)
-        except ValueError:
-            raise ValueError("Error Down Payment")
+    def get_interest_rate(self) -> float:
+        return self.interest_rate
 
-    def calculate_home_affordability(self, monthly_payment, mortgage_term, interest_rate, down_payment):
-        numerator = interest_rate * math.pow((1 + interest_rate), mortgage_term)
-        denominator = math.pow((1 + interest_rate), mortgage_term) - 1
-        property_value = (monthly_payment * denominator) / numerator
-        property_value += down_payment
+    def get_loan_term(self) -> float:
+        return self.loan_term
 
-        if property_value > 0:
-            return round(property_value)
+    def get_home_affordability_price(self) -> int:
+        return self.home_affordability_price
 
-        return 0
+    # Setters
+    def set_monthly_payment(self, monthly_payment):
+        self.monthly_payment = monthly_payment
 
-    def calculate_monthly_payment(self, house_price, annual_interest_rate, loan_term_years, down_payment):
-        loan_amount = house_price - down_payment
-        monthly_interest_rate = annual_interest_rate / 100 / 12
-        total_payments = loan_term_years * 12
+    def set_down_payment(self, down_payment):
+        self.down_payment = down_payment
 
-        if monthly_interest_rate == 0:
-            monthly_payment = loan_amount / total_payments
+    def set_interest_rate(self, interest_rate):
+        self.interest_rate = interest_rate
+
+    def set_loan_term(self, loan_term):
+        self.loan_term = loan_term
+
+    def set_home_affordability_price(self, home_affordability_price):
+        self.home_affordability_price = home_affordability_price
+
+    def reset_class_variables(self):
+        """
+        Use this function to reset the class variables before repeated use of calculator.
+        """
+        self.set_monthly_payment(-1.0)
+        self.set_down_payment(-1.0)
+        self.set_interest_rate(-1.0)
+        self.set_loan_term(-1.0)
+        self.set_home_affordability_price(-1)
+
+    # Variable Checking Functions
+    def _user_inputs_are_valid(self) -> bool:
+        """
+        Use this function to verify user inputted class variables are valid.
+        """
+        class_variables = [
+            self.monthly_payment,
+            self.down_payment,
+            self.interest_rate,
+            self.loan_term
+        ]
+        if any(var == -1.0 for var in class_variables):
+            return False
+        return True
+
+    def _home_affordability_price_is_valid(self) -> bool:
+        """
+        Ue this function to verify the home affordability price is valid.
+        """
+        if self.home_affordability_price == -1:
+            return False
+        return True
+
+    @staticmethod
+    def convert_string_number_into_float(number) -> float:
+        """
+        Use this function to convert a string representing a number into a float (if possible).
+        """
+        if not number.isdigit():
+            return -1.0
+        if not float(number) >= 0:
+            return -1.0
+        return float(number)
+
+    # Calculation Functions
+    def calculate_home_affordability_price(self) -> str:
+        """
+        Use this function to calculate the maximum home price that a user can afford based on the
+        provided financial variables.
+        """
+        if self._user_inputs_are_valid() is False:
+            return "Invalid User Inputs"
+        numerator = self._calculate_numerator()
+        denominator = self._calculate_denominator()
+        loan_affordability_price = self._calculate_loan_affordability(numerator, denominator)
+        home_affordability_price = loan_affordability_price + self.down_payment
+        self.set_home_affordability_price(round(home_affordability_price))
+        if self._home_affordability_price_is_valid() is False:
+            return "Invalid Calculation From calculate_home_affordability_price() Function"
+        return str(round(home_affordability_price))
+
+    def calculate_total_home_loan_price(self) -> str:
+        """
+        Use this function to calculate the total cost of a home loan over the loan term.
+        """
+        if self._user_inputs_are_valid() is False:
+            return "Invalid User Inputs"
+        if self._home_affordability_price_is_valid() is False:
+            return "Invalid Calculation From calculate_home_affordability_price() Function"
+        monthly_payment = self._calculate_monthly_payment()
+        if monthly_payment < 0:
+            return "Invalid Calculation From _calculate_monthly_payment() Function"
+        total_home_loan_price = monthly_payment * self._convert_loan_term_length_into_months()
+        return str(round(total_home_loan_price))
+
+    def calculate_loan_principal(self) -> str:
+        """
+        Use this function to calculate the loan's principal.
+        """
+        if self._user_inputs_are_valid() is False:
+            return "Invalid User Inputs"
+        if self._home_affordability_price_is_valid() is False:
+            return "Invalid Calculation From calculate_home_affordability_price() Function"
+        loan_principal = self.home_affordability_price - self.down_payment
+        return str(round(loan_principal))
+
+    def calculate_loan_interest(self) -> str:
+        """
+        Use this function to calculate the loan's interest.
+        """
+        if self._user_inputs_are_valid() is False:
+            return "Invalid User Inputs"
+        if self._home_affordability_price_is_valid() is False:
+            return "Invalid Calculation From calculate_home_affordability_price() Function"
+        loan_interest = float(self.calculate_total_home_loan_price()) - float(self.calculate_loan_principal())
+        return str(round(loan_interest))
+
+    # Helper Functions
+    def _convert_annual_interest_rate_to_monthly_interest_rate(self):
+        """
+        Use this function to convert annual interest rate to monthly interest rate
+        """
+        return self.interest_rate / 100 / 12
+
+    def _convert_loan_term_length_into_months(self):
+        """
+        Use this function to convert loan term length from years to months
+        """
+        return self.loan_term * 12
+
+    def _calculate_numerator(self) -> float:
+        """
+        Use this function to calculate the numerator in the calculate_home_affordability_price()
+        function.
+        """
+        interest_rate = self._convert_annual_interest_rate_to_monthly_interest_rate()
+        loan_term = self._convert_loan_term_length_into_months()
+        return interest_rate * math.pow((1 + interest_rate), loan_term)
+
+    def _calculate_denominator(self) -> float:
+        """
+        Use this function to calculate the denominator in the calculate_home_affordability_price()
+        function.
+        """
+        interest_rate = self._convert_annual_interest_rate_to_monthly_interest_rate()
+        loan_term = self._convert_loan_term_length_into_months()
+        return math.pow((1 + interest_rate), loan_term) - 1
+
+    def _calculate_loan_affordability(self, numerator, denominator) -> float:
+        """
+        Use this function to calculate the loan affordability in the
+        calculate_home_affordability_price() function.
+        """
+        return (self.monthly_payment * denominator) / numerator
+
+    def _calculate_monthly_payment(self) -> float:
+        """
+        This is a helper function for calculate_total_home_loan_price that calculates an estimated
+        monthly home loan payment based on provided financial variables.
+        """
+        if self._user_inputs_are_valid() is False or self._home_affordability_price_is_valid() is False:
+            return -1.0
+        loan_amount = self.home_affordability_price - self.down_payment
+
+        interest_rate = self._convert_annual_interest_rate_to_monthly_interest_rate()
+        loan_term = self._convert_loan_term_length_into_months()
+
+        if interest_rate == 0:
+            monthly_payment = loan_amount / loan_term
         else:
             monthly_payment = loan_amount * (
-                        monthly_interest_rate * math.pow(1 + monthly_interest_rate, total_payments)) / (
-                                          math.pow(1 + monthly_interest_rate, total_payments) - 1)
+                        interest_rate * math.pow(1 + interest_rate, loan_term)) / (
+                                          math.pow(1 + interest_rate, loan_term) - 1)
 
         return monthly_payment
 
-    def calculate_total_home_loan_price(self, house_price, annual_interest_rate, loan_term_years, down_payment):
-        monthly_payment = self.calculate_monthly_payment(house_price, annual_interest_rate, loan_term_years, down_payment)
-        total_payments = loan_term_years * 12
-        total_price = monthly_payment * total_payments
-        return total_price
-
-    def get_loan_principal(self, house_price, down_payment):
-        return house_price - down_payment
-
-    def get_loan_interest(self, house_price, annual_interest_rate, loan_term_years, down_payment):
-        total_price = self.calculate_total_home_loan_price(house_price, annual_interest_rate, loan_term_years, down_payment)
-        principal = self.get_loan_principal(house_price, down_payment)
-        loan_interest = total_price - principal
-        return loan_interest
 
 if __name__ == "__main__":
     calculator = AffordabilityCalculator()
--- home-choice-pro/tests/test_models/test_affordability_calculator.py
+++ home-choice-pro/tests/test_models/test_affordability_calculator.py
@@ -7,70 +7,162 @@ def calculator():
     return AffordabilityCalculator()
 
 
-def test_parse_interest_rate(calculator):
-    assert calculator.parse_interest_rate("5.0") == pytest.approx(0.004166666666666667)
-    assert calculator.parse_interest_rate("12") == pytest.approx(0.01)
-    assert calculator.parse_interest_rate("0") == pytest.approx(0.0)
+def test_getters_and_setters():
+    calculator = AffordabilityCalculator()
+
+    calculator.set_monthly_payment(1500.0)
+    assert calculator.get_monthly_payment() == 1500.0
+
+    calculator.set_down_payment(20000.0)
+    assert calculator.get_down_payment() == 20000.0
+
+    calculator.set_interest_rate(5)
+    assert calculator.get_interest_rate() == 5
+
+    calculator.set_loan_term(30)
+    assert calculator.get_loan_term() == 30
+
+    calculator.set_home_affordability_price(300000)
+    assert calculator.get_home_affordability_price() == 300000
 
 
-def test_parse_interest_rate_failure(calculator):
-    with pytest.raises(ValueError):
-        calculator.parse_interest_rate("abc")
+def test_reset_class_variables():
+    calculator = AffordabilityCalculator()
+    calculator.set_monthly_payment(1500.0)
+    calculator.set_down_payment(20000.0)
+    calculator.set_interest_rate(5)
+    calculator.set_loan_term(30)
+    calculator.set_home_affordability_price(300000)
+
+    calculator.reset_class_variables()
+
+    assert calculator.get_monthly_payment() == -1.0
+    assert calculator.get_down_payment() == -1.0
+    assert calculator.get_interest_rate() == -1.0
+    assert calculator.get_loan_term() == -1.0
+    assert calculator.get_home_affordability_price() == -1
 
 
-def test_parse_term(calculator):
-    assert calculator.parse_term(0) == 180
-    assert calculator.parse_term(1) == 360
-    assert calculator.parse_term(2) == 360  # any non-zero index should return 360
+def test_user_inputs_are_valid():
+    calculator = AffordabilityCalculator()
+    calculator.set_monthly_payment(1500.0)
+    calculator.set_down_payment(30000.0)
+    calculator.set_interest_rate(3.5)
+    calculator.set_loan_term(30)
+
+    assert calculator._user_inputs_are_valid() == True
+
+    calculator.set_monthly_payment(-1.0)
+    assert calculator._user_inputs_are_valid() == False
 
 
-def test_parse_term_failure(calculator):
-    with pytest.raises(ValueError):
-        calculator.parse_interest_rate("abc")
+def test_home_affordability_price_is_valid():
+    calculator = AffordabilityCalculator()
+    calculator.set_home_affordability_price(300000)
+
+    assert calculator._home_affordability_price_is_valid() == True
+
+    calculator.set_home_affordability_price(-1)
+    assert calculator._home_affordability_price_is_valid() == False
 
 
-def test_parse_monthly_payment(calculator):
-    assert calculator.parse_monthly_payment("1500") == pytest.approx(1500.0)
-    assert calculator.parse_monthly_payment("0") == pytest.approx(0.0)
+def test_convert_string_number_into_float():
+    assert AffordabilityCalculator.convert_string_number_into_float("1000") == 1000.0
+    assert AffordabilityCalculator.convert_string_number_into_float("-1000") == -1.0
+    assert AffordabilityCalculator.convert_string_number_into_float("abc") == -1.0
 
 
-def test_parse_monthly_payment_failure(calculator):
-    with pytest.raises(ValueError):
-        calculator.parse_monthly_payment("abc") == pytest.approx(0.0)
+def test_calculate_home_affordability_price():
+    calculator = AffordabilityCalculator()
+    calculator.set_monthly_payment(1500.0)
+    calculator.set_down_payment(20000.0)
+    calculator.set_interest_rate(5)
+    calculator.set_loan_term(30)
+
+    result = calculator.calculate_home_affordability_price()
+    assert result != "Invalid User Inputs"
+    assert result != "Invalid Calculation From calculate_home_affordability_price() Function"
+    assert calculator.calculate_home_affordability_price() == "299422"
 
 
-def test_parse_down_payment(calculator):
-    assert calculator.parse_down_payment("20000") == pytest.approx(20000.0)
-    assert calculator.parse_down_payment("0") == pytest.approx(0.0)
+def test_calculate_total_home_loan_price():
+    calculator = AffordabilityCalculator()
+    calculator.set_monthly_payment(1500.0)
+    calculator.set_down_payment(20000.0)
+    calculator.set_interest_rate(5)
+    calculator.set_loan_term(30)
+    calculator.calculate_home_affordability_price()
+
+    result = calculator.calculate_total_home_loan_price()
+    assert result != "Invalid User Inputs"
+    assert result != "Invalid Calculation From calculate_home_affordability_price() Function"
+    assert result != "Invalid Calculation From _calculate_monthly_payment() Function"
+    assert calculator.calculate_total_home_loan_price() == "539999"
 
 
-def test_parse_down_payment_failure(calculator):
-    with pytest.raises(ValueError):
-        calculator.parse_down_payment("abc") == pytest.approx(0.0)
+def test_calculate_loan_principal():
+    calculator = AffordabilityCalculator()
+    calculator.set_monthly_payment(1500.0)
+    calculator.set_down_payment(20000.0)
+    calculator.set_interest_rate(5)
+    calculator.set_loan_term(30)
+    calculator.calculate_home_affordability_price()
+
+    result = calculator.calculate_loan_principal()
+    assert result != "Invalid User Inputs"
+    assert result != "Invalid Calculation From calculate_home_affordability_price() Function"
+    assert calculator.calculate_loan_principal() == "279422"
 
 
-def test_calculate_home_affordability(calculator):
-    assert calculator.calculate_home_affordability(1500, 360, 0.004166666666666667, 20000) == 299422
-    assert calculator.calculate_home_affordability(1500, 180, 0.004166666666666667, 20000) == 209683
-    assert calculator.calculate_home_affordability(0, 360, 0.004166666666666667, 20000) == 20000
+def test_calculate_loan_interest():
+    calculator = AffordabilityCalculator()
+    calculator.set_monthly_payment(1500.0)
+    calculator.set_down_payment(20000.0)
+    calculator.set_interest_rate(5)
+    calculator.set_loan_term(30)
+    calculator.calculate_home_affordability_price()
+
+    result = calculator.calculate_loan_interest()
+    assert result != "Invalid User Inputs"
+    assert result != "Invalid Calculation From calculate_home_affordability_price() Function"
+    assert calculator.calculate_loan_interest() == "260577"
 
 
-def test_calculate_monthly_payment(calculator):
-    assert calculator.calculate_monthly_payment(400000, 5, 30, 20000) == pytest.approx(2039.92, 0.01)
-    assert calculator.calculate_monthly_payment(300000, 3.5, 15, 50000) == pytest.approx(1785.91, 0.01)
-    assert calculator.calculate_monthly_payment(200000, 0, 30, 50000) == pytest.approx(416.67, 0.01)
+def test_calculate_numerator():
+    calculator = AffordabilityCalculator()
+    calculator.set_interest_rate(0.035)
+    calculator.set_loan_term(30)
+
+    result = calculator._calculate_numerator()
+    assert isinstance(result, float)
 
 
-def test_calculate_total_home_loan_price(calculator):
-    assert calculator.calculate_total_home_loan_price(400000, 5, 30, 20000) == pytest.approx(734371.98, 0.01)
-    assert calculator.calculate_total_home_loan_price(300000, 3.5, 15, 50000) == pytest.approx(321464.20, 0.01)
+def test_calculate_denominator():
+    calculator = AffordabilityCalculator()
+    calculator.set_interest_rate(0.035)
+    calculator.set_loan_term(30)
+
+    result = calculator._calculate_denominator()
+    assert isinstance(result, float)
 
 
-def test_get_loan_principal(calculator):
-    assert calculator.get_loan_principal(400000, 20000) == 380000
-    assert calculator.get_loan_principal(300000, 50000) == 250000
+def test_calculate_loan_affordability():
+    calculator = AffordabilityCalculator()
+    calculator.set_monthly_payment(1500.0)
+    numerator = 0.035 * (1 + 0.035) ** 30
+    denominator = (1 + 0.035) ** 30 - 1
 
+    result = calculator._calculate_loan_affordability(numerator, denominator)
+    assert isinstance(result, float)
+    
 
-def test_get_loan_interest(calculator):
-    assert calculator.get_loan_interest(400000, 5, 30, 20000) == pytest.approx(354371.98, 0.01)
-    assert calculator.get_loan_interest(300000, 3.5, 15, 50000) == pytest.approx(71464.20, 0.01)
+def test_calculate_monthly_payment():
+    calculator = AffordabilityCalculator()
+    calculator.set_monthly_payment(1500.0)
+    calculator.set_down_payment(30000.0)
+    calculator.set_interest_rate(0.035)
+    calculator.set_loan_term(30)
+    calculator.calculate_home_affordability_price()
+
+    result = calculator._calculate_monthly_payment()
+    assert isinstance(result, float)
